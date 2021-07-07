4571c390a65f91599f62fd3feef772d31bc58acf
Andrey Babkin <babkinandrey2773@gmail.com>
2021-07-07 23:17:01 +0300

    Last update (maybe)

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,17 +1,33 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.getElementById("menu");
-const menuClose = document.createElement("div");
+const headerTop = document.querySelector(".header__top");
 
-menuBtn.addEventListener("click", function(){
-    if(menu.classList.contains("show")){
-        menu.classList.remove("show");
-        menuBtn.innerHTML = "<img src='dist/images/header/Menu-btn.png'>";
+document.body.addEventListener("click", function(event){
+
+    if(event.target === menuBtn.querySelector("img")){
+
+    if(!menu.classList.contains("show")){
+        menu.classList.add("show");
+        menuBtn.innerHTML = '<img src="dist/images/header/Menu-close-btn.png">';
+        
     }
+    
     else{
-           menu.classList.add("show")
-           menuBtn.innerHTML = '<img src="dist/images/header/Menu-close-btn.png">';
+            menu.classList.remove("show");
+            menuBtn.innerHTML = "<img src='dist/images/header/Menu-btn.png'>";
+        
+    }
+    
+}
+    else if(menu.classList.contains("show") && event.target !== menu && event.target !== document.querySelector(".header__top")){
+    menu.classList.remove("show");
+    menuBtn.innerHTML = "<img src='dist/images/header/Menu-btn.png'>";
     }
 })
+
+
+
+
 
 
 
