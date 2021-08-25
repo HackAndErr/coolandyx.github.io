6bdb515506c696e098d80ea0f27176bb8e5a830a
Andrey Babkin <babkinandrey2773@gmail.com>
2021-08-25 14:56:27 +0300

    upd login

--- src/js/well.js
+++ src/js/well.js
@@ -24,7 +24,25 @@ class Modal {
     
 }
 
-const login = new Modal("#login");
-document.querySelector(".login_btn").addEventListener("click", () => login.show())
+const login = new Modal("#loginWindow");
+document.querySelector(".login_btn").addEventListener("click", () => login.show());
+
+const logInField = document.querySelector("input[name='login']"); 
+const passwordField = document.querySelector("input[name='password']");
+
+document.querySelector("#log_in").addEventListener("click", async function(){
+    fetch("https://ajax.test-danit.com/api/v2/cards/login", {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ email: logInField.value, password: passwordField.value })
+})
+  .then(response => response.text())
+  .then(token => {console.log(token);
+    
+})
+
+})
 
 
